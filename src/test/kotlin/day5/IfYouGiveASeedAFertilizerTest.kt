f56e5b463f9ec282ec1d601c2c1795b0fb862bb6
Carst Tankink <ctankink@bol.com>
2023-12-05 20:05:22 +0100

    AoC-2023 Clean test method

--- src/test/kotlin/day5/IfYouGiveASeedAFertilizerTest.kt
+++ src/test/kotlin/day5/IfYouGiveASeedAFertilizerTest.kt
@@ -15,32 +15,27 @@ class IfYouGiveASeedAFertilizerTest {
     @Test
     fun testRangeSplits() {
         val entry: Pair<Long, Long> = 100L to 104L
-        val source4 = MapLine(104L to 103, 1)
-        val fullyBefore = splitRanges(entry, source4.sourceRange)
+        val fullyBefore = splitRanges(entry, 104L to 103)
         assertEquals(entry, fullyBefore.first)
         assertNull(fullyBefore.second)
         assertNull(fullyBefore.third)
 
-        val source3 = MapLine(99L to 100, 1)
-        val fullyAfter = splitRanges(entry, source3.sourceRange)
+        val fullyAfter = splitRanges(entry, 99L to 100)
         assertEquals(entry, fullyAfter.third)
         assertNull(fullyAfter.second)
         assertNull(fullyAfter.first)
 
-        val source2 = MapLine(99L to 106, 2)
-        val fullyIn = splitRanges(entry, source2.sourceRange)
+        val fullyIn = splitRanges(entry, 99L to 106)
         assertNull(fullyIn.first)
         assertEquals(100L to 104L, fullyIn.second)
         assertNull(fullyIn.third)
 
-        val source1 = MapLine(101L to 106, -20)
-        val overlapsStart = splitRanges(entry, source1.sourceRange)
+        val overlapsStart = splitRanges(entry, 101L to 106)
         assertEquals(100L to 101L, overlapsStart.first)
         assertEquals(101L to 104L, overlapsStart.second)
         assertNull(overlapsStart.third)
 
-        val source = MapLine(99L to 101L, 10)
-        val overlapsEnd = splitRanges(entry, source.sourceRange)
+        val overlapsEnd = splitRanges(entry, 99L to 101L)
         assertNull(overlapsEnd.first)
         assertEquals(100L to 101L, overlapsEnd.second)
         assertEquals(101L to 104L, overlapsEnd.third)
