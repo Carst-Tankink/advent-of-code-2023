7514e662b6b6571aa2d583a29e492c6aa4e79293
Carst Tankink <ctankink@bol.com>
2023-12-05 19:42:23 +0100

    AoC-2023 Day 5 star 2
    Geez

--- src/main/kotlin/day5/IfYouGiveASeedAFertilizer.kt
+++ src/main/kotlin/day5/IfYouGiveASeedAFertilizer.kt
@@ -15,8 +15,9 @@ enum class MapType : AlmanacLine {
     HUMIDITY_TO_LOCATION,
 }
 
-data class MapLine(val destinationStart: Long, val sourceStart: Long, val range: Long) : AlmanacLine
+data class MapLine(val sourceRange: Range, val offset: Long) : AlmanacLine
 data object EMPTY : AlmanacLine
+typealias Range = Pair<Long, Long>
 
 class IfYouGiveASeedAFertilizer(fileName: String?) : Solution<AlmanacLine, Long>(fileName) {
     override fun parse(line: String): AlmanacLine? {
@@ -36,7 +37,7 @@ class IfYouGiveASeedAFertilizer(fileName: String?) : Solution<AlmanacLine, Long>
             line.startsWith("humidity-to-location") -> MapType.HUMIDITY_TO_LOCATION
             numbersRegex.matches(line) -> {
                 val entry = line.split(" ").map { it.toLong() }
-                MapLine(entry[0], entry[1], entry[2])
+                MapLine(entry[1] to entry[1] + entry[2], entry[0] - entry[1])
             }
 
             else -> EMPTY
@@ -51,26 +52,32 @@ class IfYouGiveASeedAFertilizer(fileName: String?) : Solution<AlmanacLine, Long>
     }
 
     override fun solve2(data: List<AlmanacLine>): Long {
-        val seeds = expandSeeds(data.first() as SeedsLine)
+        val seeds = expandSeedsToRanges(data.first() as SeedsLine)
+
         val maps = buildMaps(emptyMap(), data.drop(2), null)
-        return seeds.minOf {
-            locationForSeeds(it, maps)
+        val locations = seeds.flatMap { seed ->
+            maps[MapType.SEED_TO_SOIL]!!.lookupRange(seed)
+                .flatMap { maps[MapType.SOIL_TO_FERTILIZER]!!.lookupRange(it) }
+                .flatMap { maps[MapType.FERTILIZER_TO_WATER]!!.lookupRange(it) }
+                .flatMap { maps[MapType.WATER_TO_LIGHT]!!.lookupRange(it) }
+                .flatMap { maps[MapType.LIGHT_TO_TEMPERATURE]!!.lookupRange(it) }
+                .flatMap { maps[MapType.TEMPERATURE_TO_HUMIDITY]!!.lookupRange(it) }
+                .flatMap { maps[MapType.HUMIDITY_TO_LOCATION]!!.lookupRange(it) }
+        }.sortedBy { it.first }
+        return seeds.minOf { seed ->
+            val ranges = maps[MapType.SEED_TO_SOIL]!!.lookupRange(seed)
+                .flatMap { maps[MapType.SOIL_TO_FERTILIZER]!!.lookupRange(it) }
+                .flatMap { maps[MapType.FERTILIZER_TO_WATER]!!.lookupRange(it) }
+                .flatMap { maps[MapType.WATER_TO_LIGHT]!!.lookupRange(it) }
+                .flatMap { maps[MapType.LIGHT_TO_TEMPERATURE]!!.lookupRange(it) }
+                .flatMap { maps[MapType.TEMPERATURE_TO_HUMIDITY]!!.lookupRange(it) }
+                .flatMap { maps[MapType.HUMIDITY_TO_LOCATION]!!.lookupRange(it) }
+            ranges.minOf { it.first }
         }
     }
 
-    private fun expandSeeds(seedsLine: SeedsLine): List<Long> {
-        tailrec fun rec(remaining: List<Long>, acc: List<Long>): List<Long> {
-            return if (remaining.isEmpty()) {
-                acc
-            } else {
-                val start = remaining.first()
-                val size = remaining.drop(1).first()
-
-                rec(remaining.drop(2), acc + (start..<start + size).toList())
-            }
-        }
-
-        return rec(seedsLine.seeds, emptyList())
+    private fun expandSeedsToRanges(seedsLine: SeedsLine): List<Range> {
+        return seedsLine.seeds.windowed(2, 2).map { Pair(it[0], it[0] + it[1]) }
     }
 
     private tailrec fun buildMaps(
@@ -82,7 +89,12 @@ class IfYouGiveASeedAFertilizer(fileName: String?) : Solution<AlmanacLine, Long>
             acc + current!!
         } else {
             when (val nextLine = remaining.first()) {
-                is MapLine -> buildMaps(acc, remaining.drop(1), Pair(current!!.first, current.second + nextLine))
+                is MapLine -> buildMaps(
+                    acc,
+                    remaining.drop(1),
+                    Pair(current!!.first, (current.second + nextLine).sortedBy { it.sourceRange.first }),
+                )
+
                 EMPTY -> buildMaps(acc + current!!, remaining.drop(1), current)
                 is MapType -> buildMaps(acc, remaining.drop(1), Pair(nextLine, emptyList()))
                 is SeedsLine -> error("Unexpected SeedsLine")
@@ -91,21 +103,76 @@ class IfYouGiveASeedAFertilizer(fileName: String?) : Solution<AlmanacLine, Long>
     }
 
     private fun locationForSeeds(seed: Long, maps: Map<MapType, List<MapLine>>): Long {
-        val soil = maps[MapType.SEED_TO_SOIL]!!.lookup(seed)
-        val fertilizer = maps[MapType.SOIL_TO_FERTILIZER]!!.lookup(soil)
-        val water = maps[MapType.FERTILIZER_TO_WATER]!!.lookup(fertilizer)
-        val light = maps[MapType.WATER_TO_LIGHT]!!.lookup(water)
-        val temperature = maps[MapType.LIGHT_TO_TEMPERATURE]!!.lookup(light)
-        val humidity = maps[MapType.TEMPERATURE_TO_HUMIDITY]!!.lookup(temperature)
-        return maps[MapType.HUMIDITY_TO_LOCATION]!!.lookup(humidity)
+        return maps[MapType.SEED_TO_SOIL]!!.lookupRange(Pair(seed, seed))
+            .flatMap { maps[MapType.SOIL_TO_FERTILIZER]!!.lookupRange(it) }
+            .flatMap { maps[MapType.FERTILIZER_TO_WATER]!!.lookupRange(it) }
+            .flatMap { maps[MapType.WATER_TO_LIGHT]!!.lookupRange(it) }
+            .flatMap { maps[MapType.LIGHT_TO_TEMPERATURE]!!.lookupRange(it) }
+            .flatMap { maps[MapType.TEMPERATURE_TO_HUMIDITY]!!.lookupRange(it) }
+            .flatMap { maps[MapType.HUMIDITY_TO_LOCATION]!!.lookupRange(it) }
+            .first().first
     }
 }
 
 fun List<MapLine>.lookup(entry: Long): Long {
     fun lookupEntry(line: MapLine): Long? {
-        val delta = line.destinationStart - line.sourceStart
-        return if (entry >= line.sourceStart && entry < (line.sourceStart + line.range)) entry + delta else null
+        val delta = line.offset
+        return if (entry >= line.sourceRange.first && entry < line.sourceRange.second) entry + delta else null
     }
 
     return this.firstNotNullOfOrNull { lookupEntry(it) } ?: entry
 }
+
+fun List<MapLine>.lookupRange(entry: Range): List<Range> {
+    tailrec fun rec(todo: List<MapLine>, acc: List<Range>, e: Range?): List<Range> {
+        return if (todo.isEmpty() || e == null) {
+            acc + listOfNotNull(e)
+        } else {
+            val firstLine = todo.first()
+            val (f, m, l) = mapSingleLine(e, firstLine)
+            rec(todo.drop(1), acc + listOfNotNull(f, m), l)
+        }
+    }
+
+    return rec(this, emptyList(), entry)
+}
+
+fun mapSingleLine(entry: Range, line: MapLine): Triple<Range?, Range?, Range?> {
+    val source = line.sourceRange
+    return when {
+        entry.second <= source.first -> Triple(entry, null, null)
+        entry.first >= source.second -> Triple(null, null, entry)
+        entry.first >= source.first && entry.second <= source.second -> Triple(
+            null,
+            entry + line.offset,
+            null,
+        )
+
+        entry.first >= source.first && entry.second > source.second -> Triple(
+            null,
+            Pair(entry.first, source.second) + line.offset,
+            Pair(source.second, entry.second),
+        )
+
+
+        entry.second > source.first && entry.second <= source.second -> {
+            Triple(
+                entry.first to source.first,
+                (source.first to entry.second) + line.offset,
+                null,
+            )
+        }
+
+        entry.first > source.first && entry.first < source.second -> {
+            Triple(
+                null,
+                (entry.first to source.second) + line.offset,
+                (source.second to entry.second),
+            )
+        }
+
+        else -> TODO()
+    }
+}
+
+operator fun Pair<Long, Long>.plus(offset: Long): Pair<Long, Long> = Pair(this.first + offset, this.second + offset)
--- src/test/kotlin/day5/IfYouGiveASeedAFertilizerTest.kt
+++ src/test/kotlin/day5/IfYouGiveASeedAFertilizerTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class IfYouGiveASeedAFertilizerTest {
-    val ifYouGiveASeedAFertilizer = IfYouGiveASeedAFertilizer("/day5/sample1/")
+    private val ifYouGiveASeedAFertilizer = IfYouGiveASeedAFertilizer("/day5/sample1/")
 
     @Test
     fun testSample1() {
@@ -15,8 +15,8 @@ class IfYouGiveASeedAFertilizerTest {
     @Test
     fun testLookup() {
         val lines = listOf(
-            MapLine(50, 98, 2),
-            MapLine(52, 50, 48),
+            MapLine(98L to 100, 50 - 98),
+            MapLine(50L to 98, 52 - 50),
         )
 
         assertEquals(0, lines.lookup(0))
@@ -29,5 +29,34 @@ class IfYouGiveASeedAFertilizerTest {
         assertEquals(50, lines.lookup(98))
         assertEquals(51, lines.lookup(99))
         assertEquals(100, lines.lookup(100))
+    }
+
+    @Test
+    fun testRangeSplits() {
+        val entry: Pair<Long, Long> = 100L to 104L
+        val fullyBefore = mapSingleLine(entry, MapLine(104L to 103, 1))
+        assertEquals(entry, fullyBefore.first)
+        assertNull(fullyBefore.second)
+        assertNull(fullyBefore.third)
+
+        val fullyAfter = mapSingleLine(entry, MapLine(99L to 100, 1))
+        assertEquals(entry, fullyAfter.third)
+        assertNull(fullyAfter.second)
+        assertNull(fullyAfter.first)
+
+        val fullyIn = mapSingleLine(entry, MapLine(99L to 106, 2))
+        assertNull(fullyIn.first)
+        assertEquals(102L to 106L, fullyIn.second)
+        assertNull(fullyIn.third)
+
+        val overlapsStart = mapSingleLine(entry, MapLine(101L to 106, -20))
+        assertEquals(100L to 101L, overlapsStart.first)
+        assertEquals(81L to 84L, overlapsStart.second)
+        assertNull(overlapsStart.third)
+
+        val overlapsEnd = mapSingleLine(entry, MapLine(99L to 101L, 10))
+        assertNull(overlapsEnd.first)
+        assertEquals(110L to 111L, overlapsEnd.second)
+        assertEquals(101L to 104L, overlapsEnd.third)
     }
 }
