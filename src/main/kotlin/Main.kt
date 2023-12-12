63bd85f73d00bb9c957aaedee8f642c554c761a4
Carst Tankink <ctankink@bol.com>
2023-12-12 07:49:21 +0100

    AoC-2023 Remove override on day function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ import java.time.LocalDateTime
 import kotlin.time.measureTime
 
 fun main() {
-    val day = getCurrentDay(10)
+    val day = getCurrentDay()
     solveDay(day) { s -> getConstructorOfDay(day).newInstance(s) }
 }
 
