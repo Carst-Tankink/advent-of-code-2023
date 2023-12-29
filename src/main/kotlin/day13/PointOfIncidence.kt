3ebf784eb67eae87cf5b7bc7dd275a217064f7dc
Carst Tankink <ctankink@bol.com>
2023-12-29 20:15:15 +0100

    AoC-2023 Day 13 Star 2
    Finally found my logical error

--- src/main/kotlin/day13/PointOfIncidence.kt
+++ src/main/kotlin/day13/PointOfIncidence.kt
@@ -7,26 +7,26 @@ enum class Terrain {
     ASH, ROCK
 }
 
-class PointOfIncidence(fileName: String?) : Solution<List<Terrain>, Long>(fileName) {
-    override fun parse(line: String): List<Terrain>? {
+class PointOfIncidence(fileName: String?) : Solution<List<Terrain>, Int>(fileName) {
+    override fun parse(line: String): List<Terrain> {
         return line.map {
             when (it) {
                 '.' -> Terrain.ASH
                 '#' -> Terrain.ROCK
-                else -> error("Unexpected characted $it")
+                else -> error("Unexpected character $it")
             }
         }
     }
 
-    override fun solve1(data: List<List<Terrain>>): Long {
+    override fun solve1(data: List<List<Terrain>>): Int {
         return groupFields(data).sumOf { mirrorPosition(it) }
     }
 
-    private fun mirrorPosition(terrain: List<List<Terrain>>): Long {
+    private fun mirrorPosition(terrain: List<List<Terrain>>): Int {
         val horizontalMirror = findMirror(terrain)
         val verticalMirror = if (horizontalMirror == 0) findMirror(terrain.transpose()) else 0
 
-        return verticalMirror.toLong() + 100 * horizontalMirror
+        return verticalMirror + 100 * horizontalMirror
     }
 
     private fun findMirror(terrain: List<List<Terrain>>) =
@@ -47,17 +47,17 @@ class PointOfIncidence(fileName: String?) : Solution<List<Terrain>, Long>(fileNa
         return folded.first + listOf(folded.second)
     }
 
-    override fun solve2(data: List<List<Terrain>>): Long {
+    override fun solve2(data: List<List<Terrain>>): Int {
         return groupFields(data).sumOf { mirrorPositionWithSmudges(it) }
 
 
     }
 
-    private fun mirrorPositionWithSmudges(field: List<List<Terrain>>): Long {
+    private fun mirrorPositionWithSmudges(field: List<List<Terrain>>): Int {
         val horizontalMirror = findMirrorWithSmudge(field)
         val verticalMirror = if (horizontalMirror == 0) findMirrorWithSmudge(field.transpose()) else 0
 
-        return verticalMirror.toLong() + 100 * horizontalMirror
+        return verticalMirror + 100 * horizontalMirror
     }
 
     private fun findMirrorWithSmudge(terrain: List<List<Terrain>>) =
@@ -65,6 +65,9 @@ class PointOfIncidence(fileName: String?) : Solution<List<Terrain>, Long>(fileNa
             val before = terrain.take(it).reversed()
             val after = terrain.drop(it)
 
-            before.zip(after).count { (l1, l2) -> l1.zip(l2).count { (t1, t2) -> t1 != t2 } == 1 } == 1
+            val differences = before.zip(after)
+                .map { (l1, l2) -> l1.zip(l2).count { (t1, t2) -> t1 != t2 } }
+
+            differences.sum() == 1
         } ?: 0
 }
